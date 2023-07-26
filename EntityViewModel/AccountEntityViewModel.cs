0b60ea9ec5d866c8ea3826e4fd5f25a48fe65a75
bug1422 <binlambaobao142@gmail.com>
2023-07-26 19:32:04 +0700

    dob, cred

--- EntityViewModel/AccountEntityViewModel.cs
+++ EntityViewModel/AccountEntityViewModel.cs
@@ -6,10 +6,12 @@ public class AccountEntityViewModel
 {
     public int accountId { get; set; }
     public string fullName { get; set; }
+    public DateTime dob { get; set; }
     public string email { get; set; }
     public string roleId { get; set; }
     public string address { get; set; }
     public string phoneNo { get; set; }
+    public int credibilityPoint { get; set; }
     public int pointBalance { get; set; }
     public bool isActive { get; set; }
 }
